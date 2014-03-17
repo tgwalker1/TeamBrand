906b896d335ade11049181dfda700272628dcc58
Reto Müller <muellerreto@hotmail.com>
2014-03-17 09:44:36 +0100

    Added Timer.c and .h

---
+++ Common/source/Timer.c
@@ -0,0 +1,23 @@
+/*
+ * Timer.c
+ *
+ *  Created on: Mar 11, 2014
+ *      Author: Reto
+ */
+
+#include "platform.h"
+
+/*! \brief Function called from timer interrupt every TMR_TICK_MS. */
+void TMR_OnInterrupt(void){
+	
+}
+
+/*! \brief Timer driver initialization */
+void TMR_Init(void){
+	
+}
+
+/*! \brief Timer driver de-initialization */
+void TMR_Deinit(void){
+	
+}
