1158bb1bc411ab6925e59128eadf519b338323e9
Andreas Walker <andreas.walker@stud.hslu.ch>
2014-03-17 10:36:38 +0100

    edited platform.h

--- Common/include/Platform.h
+++ Common/include/Platform.h
@@ -25,15 +25,25 @@
   /*!< Set to 1 if we have LEDs available, 0 otherwise */
 #define PL_HAS_EVENTS         (1)
   /*!< Set to 1 if we have the Event module implemented, 0 otherwise */
-#define PL_HAS_TIMERS         (1)
-  /*!< Set to 1 if we use Timers in our CPU, 0 otherwise */
+#define PL_HAS_TIMER          (1)
+  /*!< Set to 1 if we have the Timer module implemented, 0 otherwise */
+#define PL_HAS_LED_HEARTBEAT  (1)
+  /*!< Set to 1 if we have a LED heartbeat, 0 otherwise */
+#define PL_HAS_KEYS           (1)
+  /*!< Set to 1 if we have push buttons (keys) implemented, 0 otherwise */
+#define PL_HAS_KBI            (PL_IS_SRB)
+  /*!< Set to 1 if we have use keyboard interrupt */
 
 #if PL_IS_FRDM
-  #define PL_NOF_LEDS 3
+  #define PL_NOF_LEDS       3
      /*!< FRDM board has 3 LEDs (RGB) */
+  #define PL_NOF_KEYS       1
+     /*!< Number of push buttons on the SRB board */
 #elif PL_IS_SRB
-  #define PL_NOF_LEDS 5
+  #define PL_NOF_LEDS       5
      /*!< We have 5 LED's on the SRB board */
+  #define PL_NOF_KEYS       4
+     /*!< Number of push buttons on the SRB board */
 #else
   #error "unknown configuration?"
 #endif
